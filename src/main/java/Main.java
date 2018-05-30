e77178df24696e388d2d66601aa350192472e708
Michael Stuhlman <michael.stuhlman@technergetics.com>
2018-05-30 15:14:02 -0400

    Changes yo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,15 +16,27 @@ public class Main {
     BinanceApiRestClient client = factory.newRestClient();
 
     TickerStatistics tickerStatistics = client.get24HrPriceStatistics("BTCUSDT");
+
     List<Candlestick> candles1minute = client.getCandlestickBars("BTCUSDT", CandlestickInterval.ONE_MINUTE);
     List<Candlestick> candles5minute = client.getCandlestickBars("BTCUSDT", CandlestickInterval.FIVE_MINUTES);
     List<Candlestick> candles15minute = client.getCandlestickBars("BTCUSDT", CandlestickInterval.FIFTEEN_MINUTES);
     List<Candlestick> candles30minute = client.getCandlestickBars("BTCUSDT", CandlestickInterval.HALF_HOURLY);
+    List<Candlestick> candles1hour = client.getCandlestickBars("BTCUSDT", CandlestickInterval.HOURLY);
+    List<Candlestick> candles2hour = client.getCandlestickBars("BTCUSDT", CandlestickInterval.TWO_HOURLY);
+    List<Candlestick> candles4hour = client.getCandlestickBars("BTCUSDT", CandlestickInterval.FOUR_HOURLY);
+    List<Candlestick> candles8hour = client.getCandlestickBars("BTCUSDT", CandlestickInterval.EIGHT_HOURLY);
+
     Double currentPrice = Double.valueOf(client.get24HrPriceStatistics("BTCUSDT").getLastPrice());
+
     Double candles1minuteAvg = new CalcUtils().findAveragePrice(candles1minute);
     Double candles5minuteAvg = new CalcUtils().findAveragePrice(candles5minute);
     Double candles15minuteAvg = new CalcUtils().findAveragePrice(candles15minute);
     Double candles30minuteAvg = new CalcUtils().findAveragePrice(candles30minute);
+    Double candles1hourAvg = new CalcUtils().findAveragePrice(candles1hour);
+    Double candles2hourAvg = new CalcUtils().findAveragePrice(candles2hour);
+    Double candles4hourAvg = new CalcUtils().findAveragePrice(candles4hour);
+    Double candles8hourAvg = new CalcUtils().findAveragePrice(candles8hour);
+
     System.out.println();
   }
 }
