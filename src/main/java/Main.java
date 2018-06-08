16845a4748061786524f04a01de8fb14052e8e0f
Michael Stuhlman <michael.stuhlman@technergetics.com>
2018-06-08 11:03:21 -0400

    Log changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,20 @@ import org.apache.log4j.Logger;
 import utils.CalcUtils;
 
 public class Main {
-
 	final static Logger logger = Logger.getLogger(Wyatt.class);
 
 	public static void main(String[] args) {
-		if (args[0] == null || args[1] == null) System.exit(-1);
-		if (args[0].equals("") || args[1].equals("")) System.exit(-2);
-		//Wyatt.playSweetWater();
 		logger.info("Starting WYATT version 1.0.2 ...");
+		if (args[0] == null || args[1] == null) {
+			System.exit(-1);
+			logger.error("Not enough arguments have been given");
+		}
+		if (args[0].equals("") || args[1].equals("")) {
+			System.exit(-2);
+			logger.error("Arguments cannot be empty");
+		}
+		logger.error("Starting WYATT version 1.0.2 ...");
+		//Wyatt.playSweetWater();
 		for (; ; ) {
 			Wyatt dolores = new Wyatt(args[0], args[1]);
 			dolores.gatherMindData();
