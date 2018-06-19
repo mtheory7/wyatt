3bbe4eda944bf0529208337020e644baa6607e5b
Michael Stuhlman <msstuhlman@gmail.com>
2018-06-19 19:29:29 -0400

    Change logging a smidgen

--- src/main/java/mind/Wyatt.java
+++ src/main/java/mind/Wyatt.java
@@ -136,7 +136,7 @@ public class Wyatt {
         if (Double.valueOf(lastPrice.getLastPrice()) > sellPrice && trade) {
             Double z = Math.round(Double.valueOf(lastPrice.getLastPrice()) * 100.0) / 100.0;
             //WE SHOULD SELL AND BUY!
-            logger.info("\nDeciding to sell! Target price: $" + sellPrice + ". Current price: $" + z + ". Buy back price: $" + buyBack + "\n");
+            logger.info("\nDeciding to sell! Target price: $" + sellPrice + ". Current price: $" + z + ". Buy back price: $" + buyBack);
             performSellAndBuyBack(z, buyBack);
         }
     }
