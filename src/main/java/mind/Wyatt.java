becc7eba835a7724f6f78eaa658e2746046dab10
Michael Stuhlman <msstuhlman@gmail.com>
2018-06-25 18:36:57 -0400

    Attempt to turn off twitter logging

--- src/main/java/mind/Wyatt.java
+++ src/main/java/mind/Wyatt.java
@@ -33,7 +33,7 @@ import static java.lang.Math.max;
 public class Wyatt {
 
 	final static Logger logger = Logger.getLogger(Wyatt.class);
-	private static Double percentageRatio = 1.0001;
+	private static Double percentageRatio = 1.00015;
 	private static int MAX_TRADES_PER_24HOURS = 10;
 	private static CandlestickInterval[] intervalList = {
 			CandlestickInterval.ONE_MINUTE};
