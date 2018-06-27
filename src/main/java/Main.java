7d4e1f60151d459a788312068b975b9cb38399db
Michael Stuhlman <michael.stuhlman@technergetics.com>
2018-06-27 14:04:26 -0400

    Added lots of comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,7 +15,7 @@ public class Main {
 			Wyatt dolores = new Wyatt(args[0], args[1]);
 			dolores.setTwitterCreds(args[2], args[3], args[4], args[5]);
 			dolores.gatherMindData();
-			dolores.gatherPredictionData();
+			dolores.predictAndTrade();
 			dolores.printBalances();
 			new CalcUtils().sleeper(25000);
 		}
--- src/main/java/mind/Wyatt.java
+++ src/main/java/mind/Wyatt.java
@@ -31,29 +31,39 @@ import static com.binance.api.client.domain.account.NewOrder.limitSell;
 import static java.lang.Math.max;
 
 public class Wyatt {
-	final static Logger logger = Logger.getLogger(Wyatt.class);
-	private static Double percentageRatio = 1.000185;
-	private static int MAX_TRADES_PER_24HOURS = 10;
-	private static CandlestickInterval[] intervalList = {
-			CandlestickInterval.ONE_MINUTE};
+	private final static Logger logger = Logger.getLogger(Wyatt.class);
+	private static CandlestickInterval[] intervalList = { CandlestickInterval.ONE_MINUTE };
 	private static String[] tickers = {"BTCUSDT"};
-	public MindData mindData;
-	public PredictionData predictionData;
-	private BinanceApiClientFactory factory = null;
-	private BinanceApiRestClient client = null;
-	private String consumerKey = "";
-	private String consumerSecret = "";
-	private String accessToken = "";
-	private String accessTokenSecret = "";
+	private MindData mindData;
+	private PredictionData predictionData;
+	private BinanceApiRestClient client;
+	private String consumerKey;
+	private String consumerSecret;
+	private String accessToken;
+	private String accessTokenSecret;
 
+	/**
+	 * Instantiates a new instance of Wyatt's mind. It requires an API Key
+	 * and the API Key secret to pull the latest trading data, and to
+	 * execute trades.
+	 * @param binanceAPIKey The Binance API Key
+	 * @param binanceAPISecret The secret for the Binance API Key
+	 */
 	public Wyatt(String binanceAPIKey, String binanceAPISecret) {
 		mindData = new MindData();
 		predictionData = new PredictionData();
-		factory = BinanceApiClientFactory.newInstance(binanceAPIKey, binanceAPISecret);
+		BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance(binanceAPIKey, binanceAPISecret);
 		client = factory.newRestClient();
-
 	}
 
+	/**
+	 * Sets the credentials that are needed for tweeting alerts when
+	 * Wyatt decides to sell and buy back.
+	 * @param consumerKey Twitter Consumer Key
+	 * @param consumerSecret Twitter Consumer Secret
+	 * @param accessToken Twitter Access Token
+	 * @param accessTokenSecret Twitter Access Token Secret
+	 */
 	public void setTwitterCreds(String consumerKey, String consumerSecret, String accessToken, String accessTokenSecret) {
 		this.consumerKey = consumerKey;
 		this.consumerSecret = consumerSecret;
@@ -61,11 +71,16 @@ public class Wyatt {
 		this.accessTokenSecret = accessTokenSecret;
 	}
 
+	/**
+	 * Logs the latest balances of the Binance account. This is useful
+	 * when diagnosing trading patterns and trade logic.
+	 */
 	public void printBalances() {
 		Account account = client.getAccount();
+		//Pull the latest account balance info from Binance
 		List<AssetBalance> balances = account.getBalances();
-
 		for (AssetBalance balance : balances) {
+			//Combine the amount of idle assets, and the amount in trade currently
 			Double amount = Double.valueOf(balance.getFree()) + Double.valueOf(balance.getLocked());
 			if (amount > 0.0) {
 				logger.trace("Asset: " + balance.getAsset() + " - Balance: " + amount);
@@ -73,6 +88,10 @@ public class Wyatt {
 		}
 	}
 
+	/**
+	 * Retrieves data from the ticker data pulled from Binance. This data
+	 * is then used later for predicting a selling price.
+	 */
 	public void gatherMindData() {
 		for (String ticker : tickers) {
 			for (CandlestickInterval interval : intervalList) {
@@ -82,7 +101,12 @@ public class Wyatt {
 		}
 	}
 
-	public void gatherPredictionData() {
+	/**
+	 * Use the gathered data to attempt to predict a price to sell at, and
+	 * then a price to buy back at. When the price exceeds that target value,
+	 * perform a sell and a buy back to make an incremental amount of money.
+	 */
+	public void predictAndTrade() {
 		List<Candlestick> oneMinuteCandles = null;
 		for (HashMap.Entry<DataIdentifier, List<Candlestick>> entry : mindData.getCandlestickData().entrySet()) {
 			if (entry.getKey().getInterval() == CandlestickInterval.ONE_MINUTE
@@ -91,6 +115,7 @@ public class Wyatt {
 			}
 		}
 		List<List<Candlestick>> minuteData = new ArrayList<List<Candlestick>>();
+		//Separate out some lists of varying lengths of candles
 		if (oneMinuteCandles != null) {
 			int x = oneMinuteCandles.size() - 5;
 			int y = oneMinuteCandles.size();
@@ -105,6 +130,7 @@ public class Wyatt {
 			minuteData.add(oneMinuteCandles.subList(x, y));
 			minuteData.add(oneMinuteCandles.subList(0, y));
 		}
+		//Now use the candle lists to calculate average data used for price prediction
 		for (List<Candlestick> list : minuteData) {
 			Double openAvg = 0.0;
 			Double closeAvg = 0.0;
@@ -129,39 +155,47 @@ public class Wyatt {
 		Double tierThr = 0.0;
 		Double tierFou = 0.0;
 		Double tierFiv = 0.0;
+		//Calculate averages, and use those and a ratio to create tiered target prices
 		for (AverageData averageData : predictionData.averageData) {
-			//total += (averageData.getCloseAvg()+averageData.getHighAvg())/2*percentageRatio;
+			Double TARGET_PERCENT_RATIO = 1.000185;
 			if (averageData.getNumberOfNodesAveraged() == 5)
-				tierOne += (averageData.getCloseAvg() + averageData.getHighAvg()) / 2 * percentageRatio;
+				tierOne += (averageData.getCloseAvg() + averageData.getHighAvg()) / 2 * TARGET_PERCENT_RATIO;
 			if (averageData.getNumberOfNodesAveraged() == 25)
-				tierTwo += (averageData.getCloseAvg() + averageData.getHighAvg()) / 2 * percentageRatio;
+				tierTwo += (averageData.getCloseAvg() + averageData.getHighAvg()) / 2 * TARGET_PERCENT_RATIO;
 			if (averageData.getNumberOfNodesAveraged() == 100)
-				tierThr += (averageData.getCloseAvg() + averageData.getHighAvg()) / 2 * percentageRatio;
+				tierThr += (averageData.getCloseAvg() + averageData.getHighAvg()) / 2 * TARGET_PERCENT_RATIO;
 			if (averageData.getNumberOfNodesAveraged() == 200)
-				tierFou += (averageData.getCloseAvg() + averageData.getHighAvg()) / 2 * percentageRatio;
+				tierFou += (averageData.getCloseAvg() + averageData.getHighAvg()) / 2 * TARGET_PERCENT_RATIO;
 			if (averageData.getNumberOfNodesAveraged() == 500)
-				tierFiv += (averageData.getCloseAvg() + averageData.getHighAvg()) / 2 * percentageRatio;
+				tierFiv += (averageData.getCloseAvg() + averageData.getHighAvg()) / 2 * TARGET_PERCENT_RATIO;
 		}
+		//Round those target prices
 		tierOne = Math.round(tierOne * 100.0) / 100.0;
 		tierTwo = Math.round(tierTwo * 100.0) / 100.0;
 		tierThr = Math.round(tierThr * 100.0) / 100.0;
 		tierFou = Math.round(tierFou * 100.0) / 100.0;
 		tierFiv = Math.round(tierFiv * 100.0) / 100.0;
+		//Find max of all the tiered target prices
 		Double target = max(tierFiv, max(tierFou, max(tierThr, max(tierOne, tierTwo))));
-		Double buyBack = Math.round(target * predictionData.buyBackAfterThisPercentage * 100.0) / 100.0;
+		//Calculate the buy back price using a configurable buy back percentage ratio
+		Double buyBack = Math.round(target * PredictionData.buyBackAfterThisPercentage * 100.0) / 100.0;
 		TickerStatistics lastPrice = null;
+		//Pull the latest value from mindData
 		for (HashMap.Entry<DataIdentifier, TickerStatistics> entry : mindData.getLastPriceData().entrySet()) {
 			if (entry.getKey().getInterval() == CandlestickInterval.ONE_MINUTE
 					&& entry.getKey().getTicker().equals("BTCUSDT")) {
 				lastPrice = entry.getValue();
 			}
 		}
-		Double lastPriceFloored = Math.round(Double.valueOf(lastPrice.getLastPrice()) * 100.0) / 100.0;
-		Double sellConfidencePercentage = (lastPriceFloored / target * 100);
-		Double sellConfidence = Math.round(sellConfidencePercentage * 1000.0) / 1000.0;
+		Double lastPriceFloored = 0.0;
+		if (lastPrice != null && lastPrice.getLastPrice() != null) {
+			//Round the last price that was just pulled
+			lastPriceFloored = Math.round(Double.valueOf(lastPrice.getLastPrice()) * 100.0) / 100.0;
+		}
+		//Calculate and round sell confidence percentage
+		Double sellConfidence = Math.round((lastPriceFloored / target * 100) * 1000.0) / 1000.0;
 		logger.trace("Current: $" + lastPriceFloored + " Target: $" + target + " Buy back: $" + buyBack + " ::: " + sellConfidence + "%");
 		logger.trace("Tier_1: " + tierOne + " Tier_2: " + tierTwo + " Tier_3: " + tierThr + " Tier_4: " + tierFou + " Tier_5: " + tierFiv);
-		//logger.trace("LEVEL ONE: Current: $" + lastPriceFloored + " Sell: $" + sellPrice + " Buy: $" + buyBackLevelOne);
 		boolean trade = true;
 		List<Order> openOrders = client.getOpenOrders(new OrderRequest("BTCUSDT"));
 		if (openOrders.size() > 0) {
@@ -179,9 +213,17 @@ public class Wyatt {
 		}
 	}
 
+	/**
+	 * Function to connect to Binance, and pull the information in candle form
+	 * used to run the bot.
+	 * @param mindData The structure to save the data to
+	 * @param interval The interval to grab candle data for
+	 * @param ticker The ticker to grab candle data for
+	 */
 	private void gatherIntervalData(MindData mindData, CandlestickInterval interval, String ticker) {
 		List<Candlestick> candlesticks = new ArrayList<Candlestick>();
 		try {
+			//Make the GET call to Binance
 			candlesticks = client.getCandlestickBars(ticker, interval);
 		} catch (Exception e) {
 			logger.info("There was an exception while pulling interval data!");
@@ -190,20 +232,28 @@ public class Wyatt {
 			new CalcUtils().sleeper(120000);
 			logger.error("Error: ", e);
 		}
-
+		//Save the pulled data to the passed in data structure
 		mindData.candlestickData.put(new DataIdentifier(interval, ticker), candlesticks);
 		mindData.lastPriceData.put(new DataIdentifier(interval, ticker), client.get24HrPriceStatistics(ticker));
 		mindData.candlestickIntAvgData.put(new DataIdentifier(interval, ticker),
 				new CalcUtils().findAveragePrice(candlesticks));
 	}
 
-	public void performSellAndBuyBack(Double sellPrice, Double buyPrice) {
+	/**
+	 * Perform a sell and buy at the passed in values. Uses the Binance
+	 * configuration to execute these trades.
+	 * @param sellPrice Price to sell at
+	 * @param buyPrice Price to buy at
+	 */
+	private void performSellAndBuyBack(Double sellPrice, Double buyPrice) {
 		Account account = client.getAccount();
+		//Find out how much free asset there is to trade
 		Double freeBTC = Double.valueOf(account.getAssetBalance("BTC").getFree());
 		Double freeBTCFloored = Math.floor(Double.valueOf(freeBTC) * 10000.0) / 10000.0;
 		logger.info("Amount of BTC to trade: " + freeBTCFloored);
 		try {
 			logger.info("Executing sell of: " + freeBTCFloored + " BTC @ $" + sellPrice);
+			//Submit the binance sell
 			NewOrderResponse performSell = client.newOrder(
 					limitSell("BTCUSDT", TimeInForce.GTC, freeBTCFloored.toString(), sellPrice.toString()));
 			logger.info("Trade submitted: " + performSell.getTransactTime());
@@ -212,6 +262,7 @@ public class Wyatt {
 			e.printStackTrace();
 		}
 		new CalcUtils().sleeper(3000);
+		//Wait and make sure that the trade executed. If not, keep waiting
 		List<Order> openOrders = client.getOpenOrders(new OrderRequest("BTCUSDT"));
 		logger.trace("Number of open BTCUSDT orders: " + openOrders.size());
 		while (openOrders.size() > 0) {
@@ -221,6 +272,7 @@ public class Wyatt {
 		}
 		new CalcUtils().sleeper(3000);
 		account = client.getAccount();
+		//Verify that we have the correct amount of asset to trade
 		Double freeUSDT = Double.valueOf(account.getAssetBalance("USDT").getFree());
 		//Loop until above 10.0 USDT
 		while (freeUSDT < 10.0) {
@@ -228,11 +280,13 @@ public class Wyatt {
 			new CalcUtils().sleeper(15000);
 			freeUSDT = Double.valueOf(account.getAssetBalance("USDT").getFree());
 		}
-		Double freeUSDTFloored = Math.floor(Double.valueOf(freeUSDT) * 100.0) / 100.0;
+		//Calculate and round the values in preparation for buying back
+		Double freeUSDTFloored = Math.floor(freeUSDT * 100.0) / 100.0;
 		Double BTCtoBuy = freeUSDTFloored / buyPrice;
-		Double BTCtoBuyFloored = Math.floor(Double.valueOf(BTCtoBuy) * 10000.0) / 10000.0;
+		Double BTCtoBuyFloored = Math.floor(BTCtoBuy * 10000.0) / 10000.0;
 		try {
 			logger.info("Executing buy with: " + freeUSDTFloored + " USDT @ $" + buyPrice + " = " + BTCtoBuyFloored + " BTC");
+			//Submit the Binance buy back
 			NewOrderResponse performBuy = client.newOrder(
 					limitBuy("BTCUSDT", TimeInForce.GTC, BTCtoBuyFloored.toString(), buyPrice.toString()));
 			logger.info("Trade submitted: " + performBuy.getTransactTime());
@@ -243,7 +297,13 @@ public class Wyatt {
 		new CalcUtils().sleeper(3000);
 	}
 
+	/**
+	 * Function to send a tweet. Pass in the message to send and it will use
+	 * the preconfigured Twitter OAuth credentials.
+	 * @param message The message to tweet
+	 */
 	private void sendTweet(String message) {
+		//Use OAuth to pass Twitter credentials
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setDebugEnabled(true)
 				.setOAuthConsumerKey(consumerKey)
@@ -252,6 +312,7 @@ public class Wyatt {
 				.setOAuthAccessTokenSecret(accessTokenSecret);
 		TwitterFactory tf = new TwitterFactory(cb.build());
 		Twitter twitter = tf.getInstance();
+		//Tweets can only be 280 characters long error if longer
 		if (message.length() <= 280) {
 			try {
 				twitter.updateStatus(message);
