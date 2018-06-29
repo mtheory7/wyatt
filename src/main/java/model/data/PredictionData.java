e9961ddd8458cd4d8363d8ba614fe8936db92ba6
Michael Stuhlman <michael.stuhlman@technergetics.com>
2018-06-29 10:55:50 -0400

    Changes bruh

--- src/main/java/model/data/PredictionData.java
+++ src/main/java/model/data/PredictionData.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PredictionData {
-	public static Double buyBackAfterThisPercentage = 0.99;
+	public static Double buyBackAfterThisPercentage = 0.993;
 	public Double sellConfidencePercentage;
 	public Double sellPrice;
 	public List<AverageData> averageData;
