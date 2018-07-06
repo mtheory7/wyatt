cbcc585fea2e931e334ee5e0e6f18a1b87db182e
Michael Stuhlman <michael.stuhlman@technergetics.com>
2018-07-06 14:03:42 -0400

    Slight log changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	private final static Logger logger = Logger.getLogger(Main.class);
 
 	public static void main(String[] args) {
-		logger.info("Starting WYATT version 4.0.4 ...");
+		logger.info("Starting WYATT version 4.0.5 ...");
 		if (args.length < 6) {
 			logger.error("Not enough arguments have been given");
 			System.exit(-1);
