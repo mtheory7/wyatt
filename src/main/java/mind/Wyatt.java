40fe3755c1f72bf7eab8c34ea85e2d6fbee52dc8
Michael Stuhlman <michael.stuhlman@technergetics.com>
2018-07-09 15:08:24 -0400

    .

--- src/main/java/mind/Wyatt.java
+++ src/main/java/mind/Wyatt.java
@@ -96,7 +96,7 @@ public class Wyatt {
 		}
 		Double percentOnInvenstment = ((estimatedBalance / 0.0045) * 100) - 100;
 		percentOnInvenstment = Math.round(percentOnInvenstment * 100.0) / 100.0;
-		logger.trace("Estimated total account value: " + estimatedBalance);
+		logger.trace("Estimated total account value: " + estimatedBalance + " BTC");
 		logger.trace("Profit since starting (0.0045 BTC): " + percentOnInvenstment + "%");
 	}
 
