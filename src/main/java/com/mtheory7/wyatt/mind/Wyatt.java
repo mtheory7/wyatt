73b04d6181ccda3721b347e069c8fee14a960ec7
Michael Stuhlman <michael.stuhlman@technergetics.com>
2018-08-14 09:57:22 -0400

    Dev mode off

--- src/main/java/com/mtheory7/wyatt/mind/Wyatt.java
+++ src/main/java/com/mtheory7/wyatt/mind/Wyatt.java
@@ -31,7 +31,7 @@ import static com.binance.api.client.domain.account.NewOrder.*;
 
 @Service
 public class Wyatt {
-  private static final boolean DEVELOPING = true;
+  private static final boolean DEVELOPING = false;
   private static final Logger logger = Logger.getLogger(Wyatt.class);
   private static final CandlestickInterval[] intervalList = {
     CandlestickInterval.ONE_MINUTE, CandlestickInterval.THREE_MINUTES,
