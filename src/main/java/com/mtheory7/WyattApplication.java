50f72fd14b61ec1a7780ff62936bb664df59c950
Michael Stuhlman <msstuhlman@gmail.com>
2018-08-18 14:52:21 -0400

    New Status endpoint

--- src/main/java/com/mtheory7/WyattApplication.java
+++ src/main/java/com/mtheory7/WyattApplication.java
@@ -13,7 +13,7 @@ public class WyattApplication {
 
   public static void main(String[] args) {
     ConfigurableApplicationContext context = SpringApplication.run(WyattApplication.class, args);
-    logger.info("Starting WYATT (v6.2.7) ...");
+    logger.info("Starting WYATT (v6.3.0) ...");
     if (args.length < 6) {
       logger.error("Not enough arguments have been given");
       System.exit(-1);
