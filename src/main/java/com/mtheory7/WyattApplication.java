7befd63227daf4af9da98ea56c35b693c69ae997
Michael Stuhlman <msstuhlman@gmail.com>
2018-08-20 00:46:40 -0400

    Cool stuff.

--- src/main/java/com/mtheory7/WyattApplication.java
+++ src/main/java/com/mtheory7/WyattApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class WyattApplication {
   private static final Logger logger = Logger.getLogger(WyattApplication.class);
-  private static final String VERSION = "6.3.6";
+  private static final String VERSION = "6.3.7";
   public static void main(String[] args) {
     ConfigurableApplicationContext context = SpringApplication.run(WyattApplication.class, args);
     logger.info("Starting WYATT (v" + VERSION + ") ...");
--- src/main/java/com/mtheory7/controller/WyattController.java
+++ src/main/java/com/mtheory7/controller/WyattController.java
@@ -79,7 +79,24 @@ public class WyattController {
     response += "<br>Current portfolio value: " + wyatt.getCurrentBalance() + " BTC";
     response += "<br>Current profit: " + wyatt.getCurrentProfit() + "%";
     return new ResponseEntity<>(
-        "<font face=\"courier new\">" + response + "</font>", HttpStatus.OK);
+        "<html>\n"
+            + "<head>\n"
+            + "<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/apple-touch-icon.png\">\n"
+            + "<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/favicon-32x32.png\">\n"
+            + "<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/favicon-16x16.png\">\n"
+            + "<link rel=\"manifest\" href=\"/site.webmanifest\">\n"
+            + "<link rel=\"mask-icon\" href=\"/safari-pinned-tab.svg\" color=\"#5bbad5\">\n"
+            + "<meta name=\"msapplication-TileColor\" content=\"#da532c\">\n"
+            + "<meta name=\"theme-color\" content=\"#ffffff\">\n"
+            + "</head>\n"
+            + "<title>Wyatt</title>\n"
+            + "<body bgcolor=\"#000000\">\n"
+            + "<font face=\"Courier\" size=\"3\" color=\"#2fff00\">\n"
+            + response
+            + "</font> \n"
+            + "</body>\n"
+            + "</html> ",
+        HttpStatus.OK);
   }
 
   @GetMapping(path = PATH_OPEN_ORDERS)
