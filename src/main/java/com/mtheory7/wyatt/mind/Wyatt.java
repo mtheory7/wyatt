23406c5668a3b9e67cbbc069634be4d25861cc01
Michael Stuhlman <msstuhlman@gmail.com>
2018-08-26 22:33:34 -0400

    New rounding for $ amounts

--- src/main/java/com/mtheory7/wyatt/mind/Wyatt.java
+++ src/main/java/com/mtheory7/wyatt/mind/Wyatt.java
@@ -243,7 +243,7 @@ public class Wyatt {
   public String getCurrentProfit() {
     Double estimatedBalance = Double.valueOf(getCurrentBalance());
     Double percentOnInvenstment = ((estimatedBalance / INITIAL_INVESTMENT) * 100) - 100;
-    percentOnInvenstment = Math.round(percentOnInvenstment * 10000.0) / 10000.0;
+    percentOnInvenstment = Math.round(percentOnInvenstment * 1000.0) / 1000.0;
     return percentOnInvenstment.toString();
   }
 
