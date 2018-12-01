fd1f52e8a1305c23a7eeae6c4f8b0ac56362ea68
Michael Stuhlman <msstuhlman@gmail.com>
2018-12-01 16:32:37 -0500

    Formatting

--- src/main/java/com/mtheory7/wyatt/model/data/PredictionEngine.java
+++ src/main/java/com/mtheory7/wyatt/model/data/PredictionEngine.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 public class PredictionEngine {
-    public static Double buyBackAfterThisPercentage = 0.98;
+  public static Double buyBackAfterThisPercentage = 0.98;
   public Double targetPrice;
   private List<AverageData> averageData;
   private List<Double> targetPrices;
@@ -59,8 +59,8 @@ public class PredictionEngine {
               avg.getCloseAvg());
       targetPrices.add(target);
     }
-      // Calculate target price by maxing the targetPrices and add a small percentage
-      targetPrice = CalcUtils.floorTo(maxTarget(targetPrices), 2) * 1.0125;
+    // Calculate target price by maxing the targetPrices and add a small percentage
+    targetPrice = CalcUtils.floorTo(maxTarget(targetPrices), 2) * 1.0125;
   }
 
   /**
