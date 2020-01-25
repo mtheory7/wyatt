50ae2eaa208fa651f79c4271fd0a3b2e6a6b31b9
Michael Stuhlman <msstuhlman@gmail.com>
2020-01-25 07:59:59 -0600

    Adding rest endpoint for another project

--- src/main/java/com/mtheory7/controller/WyattController.java
+++ src/main/java/com/mtheory7/controller/WyattController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -204,6 +205,7 @@ public class WyattController {
         HttpStatus.OK);
   }
 
+  @CrossOrigin(origins = "*", allowedHeaders = "*")
   @GetMapping(path = PATH_PRICE_DATA)
   public ResponseEntity getPriceData() {
     return new ResponseEntity<>(
